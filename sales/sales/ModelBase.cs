410aa351bbd4f6006fdd64ece9927ef25796b387
raimu <uchihara0330@gmail.com>
2018-05-30 14:41:47 +0900

    salesのバックアップ

---
+++ sales/sales/ModelBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales
+{
+    class ModelBase: IDataErrorInfo
+    {
+        private readonly Dictionary<string, string>
+            _errors = new Dictionary<string, string>();
+
+        public virtual string Error
+        {
+            get
+            {
+                return
+                    _errors.Count > 0 ?
+                    "項目の値が不明です" :
+                    "";
+            }
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                return
+                    _errors.ContainsKey(propertyName) ?
+                    _errors[propertyName] :
+                    "";
+            }
+        }
+
+        protected void UpdateErrors(string propertyName, string errorMessage)
+        {
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                _errors.Remove(propertyName);
+            }
+            else
+            {
+                _errors[propertyName] = errorMessage;
+            }
+        }
+    }
+}
---
+++ sales/sales/SalesMonthModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using sales.Properties;
+
+
+namespace sales
+{
+    using ItemsType = ObservableCollection<Result>;
+    using sales;
+    class SalesMonthModel
+    {
+        private readonly SalesDatabaseContext _dataContext;
+        private readonly SalesMonthModel _previousModel;
+        public SalesMonthModel
+            (SalesDatabaseContext dataContext,
+            byte month,
+            SalesMonthModel previousModel)
+        {
+            _resultItems.CollectionChanged += _resultItems_CollectionChanged;
+            _dataContext = dataContext;
+            _previousModel = previousModel;
+            _month = month;
+            renew();
+        }
+        private readonly byte _month;
+        public byte Month 
+        {
+            get
+            {
+                return _month;
+            }
+        }
+        public int PlanPrice 
+        {
+            get
+            {
+                return Settings.Default.PlanPrice;
+            }
+        }
+        public int ResultPrice { get; private set; }
+        public int SubtractPrice 
+        {
+            get
+            {
+                return ResultPrice - PlanPrice;
+            }
+        }
+        public int TotalPrice 
+        {
+            get
+            {
+                int p =
+                    _previousModel == null ?
+                    0 :
+                    _previousModel.TotalPrice;
+                return p + SubtractPrice;
+            }
+        }
+        private readonly ItemsType _resultItems = new ItemsType();
+        public ItemsType ResultItems 
+        {
+            get
+            {
+                return _resultItems ;
+            }
+        }
+
+        public void renew()
+        {
+            var q =
+                from p in _dataContext.Results
+                where
+                p.Date.Year == Years.Item &&
+                p.Date.Month == Month &&
+                p.SectionId == Sections.Item.id
+                select p;
+
+            _resultItems.Clear();
+            foreach (Result x in q)
+            {
+                _resultItems.Add(x);
+            }
+                
+        }
+        public void Add(Result row)
+        {
+            _dataContext.Results.Add(row);
+            _resultItems.Add(row);
+        }
+        public void Remove(Result row)
+        {
+            _dataContext.Results.Remove(row);
+            _resultItems.Remove(row);
+        }
+
+        public int SaveChanges()
+        {
+            return _dataContext.SaveChanges();
+        }
+        private void _resultItems_CollectionChanged(
+            object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var q =
+                from p in _resultItems
+                select p.Price;
+            ResultPrice = q.Sum();
+        }
+    }
+}
